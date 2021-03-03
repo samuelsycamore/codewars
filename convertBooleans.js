16294c2336ef965b6a4bdfc4e4a244139401e7a1
samuelsycamore <tangled@Sams-MacBook-Pro.local>
2021-03-03 10:09:47 -0800

    boolToWord

---
+++ convertBooleans.js
@@ -0,0 +1,19 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// my solution
+
+function boolToWord( bool ){
+  if ( bool == true ) {
+    return "Yes"
+  } else return "No"
+}
+
+
+
+console.log(boolToWord( false ))
+
+// others on codewars
+
+function boolToWord( bool ){
+  return bool ? 'Yes':'No';
+}
